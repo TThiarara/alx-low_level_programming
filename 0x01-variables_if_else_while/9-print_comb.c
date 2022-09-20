3d7e006669ec339edceb9fda2b33a671f9ce3122
TThiarara <63654382+TThiarara@users.noreply.github.com>
2022-09-20 13:24:03 +0300

    Updated

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,25 +2,38 @@
 #include <stdlib.h>
 #include <time.h>
 
+/**
+ * main - No entry point
+ *
+ * Return: Always 0 (Success!)
+ */
+
 int main(void)
 {
 	int low;
+
+
 	for (low = 0; low <= 9; low++)
 
 	{
+
 		putchar(low + '0');
 
 		if (low < 9)
 
 		{
+
 			putchar(',');
 
 			putchar(' ');
 
-
 		}
+
 	}
+
 	putchar('\n');
 
+
 	return (0);
+
 }
